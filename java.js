5ddba53e3766d3a986ed0f9d78b8f4a19ea1e28b
sandeeppachatgmailcom <133961434+sandeeppachatgmailcom@users.noreply.github.com>
2023-05-26 05:39:01 +0530

    Add files via upload

---
+++ java.js
@@ -0,0 +1,31 @@
+
+function drilldown(){
+    var a = document.getElementById("Series");
+    a.click = true;
+}
+function myFunction() {
+    // Code to be executed when the button is clicked
+    alert("Button clicked!");
+    // Add your custom JavaScript logic here
+  }
+
+function toggleDiv() {
+    var div = document.getElementById("Minmenu");
+    
+    if (div.style.display === "none") {
+      div.style.display = "block";
+    } else {
+      div.style.display = "none";
+    }
+    
+  }
+  function displaymodel() {
+    var div = document.getElementsByClassName("classname");
+    
+    if (div.style.display === "none") {
+      div.style.display = "block";
+    } else {
+      div.style.display = "none";
+    }
+    
+  }  
